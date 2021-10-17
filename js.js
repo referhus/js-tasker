3e519027993e59fed7df867c742f368874084f11
Winterh <kotfimn@mail.ru>
2021-10-17 20:56:31 +0700

    добавление задачи и кнопки 'удалить'

--- js.js
+++ js.js
@@ -1,30 +1,41 @@
 window.addEventListener("DOMContentLoaded", () => {
 
- let body = document.querySelector('.tasker-body'),
+ let list = document.querySelector('.task-list'),
     input = document.querySelector('.tasker-input'),
-    text;
+    btn = document.querySelector('.tasker-button'),
+    task,
+    taskLabel,
+    todoList = [];
 
+  btn.addEventListener('click', newTask);
 
- input.addEventListener('change', () => {
-  text = document.querySelector('.tasker-input').value;
-  let id = createId();
-
-  if(text !== ' ') {
-   let bTask = document.createElement('div');
-   bTask.className = 'b-task';
-   let task = document.createElement('input')
-   let taskLabel = document.createElement('label')
-   task.setAttribute('type', 'checkbox')
-   task.setAttribute('id', `${id}`)
-   taskLabel.setAttribute('for', `${id}`)
-   taskLabel.innerHTML = `${text}`
-   body.append(bTask)
-   bTask.append(task)
-   bTask.append(taskLabel)
+   function newTask() {
+     list.innerHTML = ''
+     newTodo = document.querySelector('.tasker-input').value;
+     if(newTodo !== '') {
+     todoList.push(newTodo)
+     todoList.forEach(i => {
+     let id = createId();
+     let bTask = document.createElement('div');
+     let deleteTask = document.createElement('div');
+     deleteTask.className = 'delete';
+     bTask.className = 'b-task';
+     task = document.createElement('input')
+     taskLabel = document.createElement('label')
+     task.setAttribute('type', 'checkbox')
+     task.setAttribute('id', `${id}`)
+     taskLabel.setAttribute('for', `${id}`)
+     taskLabel.innerHTML = `${i}`
+     list.append(bTask)
+     bTask.append(task)
+     bTask.append(taskLabel)
+     bTask.append(deleteTask)
+     document.querySelector('.tasker-input').value = ''
+     })
+    } else {
+     console.log('Задача не введена!')
+    }
   }
- })
-
-
 
    function createId() {
     let random_start = 1;
